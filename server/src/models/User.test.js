b0adc82f325758badefbf6aac09228cc5db3f2d9
Paul Preda <ppd1512@gmail.com>
2023-05-05 13:28:09 +0100

    fixed the index.js and fixed the user.test file not running

--- server/src/models/User.test.js
+++ server/src/models/User.test.js
@@ -1,23 +1,24 @@
-const { describe, it, expect, beforeAll, afterAll } = require('@jest/globals')
-const { User } = require('./User')
-const db = require('../db/config')
+const { describe, it, expect, beforeAll, afterAll } = require('@jest/globals');
+const { User } = require('../models');
+const { sequelize } = require('../db/config');
+
 // define in global scope
 let user
-// clear db and create new user before tests
+// clear sequelize and create new user before tests
 beforeAll(async () => {
-  await db.sync({ force: true })
+  await sequelize.sync({ force: true })
   user = await User.create({ username: 'gandalf' })
 })
-// clear db after tests
-afterAll(async () => await db.sync({ force: true }))
+// clear sequelize after tests
+afterAll(async () => await sequelize.sync({ force: true }))
 describe('User', () => {
   it('has an id', async () => {
     expect(user).toHaveProperty('id')
   })
  
 })
-// clear db after tests
-afterAll(async () => await db.sync({ force: true }))
+// clear sequelize after tests
+afterAll(async () => await sequelize.sync({ force: true }))
 // check that the username of the created user is actually gandalf
 describe('User', () => {
   it('checks if username actually says gandalf', async () => {
@@ -25,8 +26,8 @@ describe('User', () => {
   })
  
 })
-// clear db after tests
-afterAll(async () => await db.sync({ force: true }))
+// clear sequelize after tests
+afterAll(async () => await sequelize.sync({ force: true }))
 // check that the username word count gandalf
 describe('User', () => {
   it('checks the length of username gandalf is 7', async () => {
@@ -34,8 +35,8 @@ describe('User', () => {
   })
  
 })
-// clear db after tests
-afterAll(async () => await db.sync({ force: true }))
+// clear sequelize after tests
+afterAll(async () => await sequelize.sync({ force: true }))
 // check that the username has been defined
 describe('User', () => {
   it('check that the username has been defined ', async () => {
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -8,13 +8,13 @@ const { Attack } = require('./Attack')
 User.hasOne(Deck, { through: "Player" })
 Deck.belongsTo(User, { through: "Player" })
 
-Deck.hasMany(Card, { through: "Deck" })
+Deck.belongsToMany(Card, { through: "Deck" })
 Card.belongsTo(Deck, { through: "Deck" })
 
-Card.hasMany(Attack, { through: "Card" })
+Card.belongsToMany(Attack, { through: "Card" })
 Attack.belongsToMany(Card, { through: "Card" })
 
 //set up the associations here
 
 // and then export them all below
-module.exports = { User }
+module.exports = { User, Deck, Card, Attack }
