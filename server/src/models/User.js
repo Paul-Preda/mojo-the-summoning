e1fe21cdf910d6eea6fcf4de85bbb4b5337e9299
Paul Preda <ppd1512@gmail.com>
2023-05-05 12:11:08 +0100

    added tests for the user

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,11 +1,8 @@
-// let Sequelize = require("sequelize");
-// let { sequelize } = require("../db/config");
-
 const Sequelize = require('sequelize');
 const { sequelize } = require('../db/config');
 
 
-// create your User model here
+// created the User model here
 const User = sequelize.define("user", {
     //id : Sequelize.INTEGER,
     username: Sequelize.STRING,
--- server/src/models/User.test.js
+++ server/src/models/User.test.js
@@ -1,26 +1,45 @@
 const { describe, it, expect, beforeAll, afterAll } = require('@jest/globals')
 const { User } = require('.')
 const db = require('../db/config')
-
 // define in global scope
 let user
-
 // clear db and create new user before tests
 beforeAll(async () => {
   await db.sync({ force: true })
   user = await User.create({ username: 'gandalf' })
 })
-
 // clear db after tests
 afterAll(async () => await db.sync({ force: true }))
-
 describe('User', () => {
   it('has an id', async () => {
     expect(user).toHaveProperty('id')
   })
-
-  /**
-   * Create more tests
-   * E.g. check that the username of the created user is actually gandalf
-   */
+ 
+})
+// clear db after tests
+afterAll(async () => await db.sync({ force: true }))
+// check that the username of the created user is actually gandalf
+describe('User', () => {
+  it('checks if username actually says gandalf', async () => {
+    expect(user.username).toBe('gandalf')
+  })
+ 
+})
+// clear db after tests
+afterAll(async () => await db.sync({ force: true }))
+// check that the username word count gandalf
+describe('User', () => {
+  it('checks the length of username gandalf is 7', async () => {
+    expect(user.username).toHaveLength(7)
+  })
+ 
+})
+// clear db after tests
+afterAll(async () => await db.sync({ force: true }))
+// check that the username  has been defines 
+describe('User', () => {
+  it('check that the username  has been defines ', async () => {
+    expect(user.username).toBeDefined();
+  })
+ 
 })
