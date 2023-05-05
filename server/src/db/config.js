4140d5d161d35a8f16370ac3d12fa434bc36de39
Paul Preda <ppd1512@gmail.com>
2023-05-05 11:27:17 +0100

    added the connection

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,9 @@
 // add your database connection here
+let Sequelize = require("sequelize")
+
+let sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: (__dirname,'db.sqlite')
+})
+
+module.exports = sequelize
