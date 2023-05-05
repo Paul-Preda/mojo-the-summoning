9a452be99943e95c8ce5958ef9c9cc0807de91e0
Paul Preda <ppd1512@gmail.com>
2023-05-05 14:19:56 +0100

    wrote index.js again after branching because it got removed

--- server/src/views/index.js
+++ server/src/views/index.js
@@ -1,4 +1,17 @@
-const user = require('./user')
-const card = require('./card')
+const user = require('../models/User')
+const card = require('../models/Card')
+const deck = require('../models/Deck')
+const attack = require('../models/Attack')
+const { Deck, User, Card, Attack } = require('../models')
 
-module.exports = { user, card }
+User.hasOne(Deck)
+Deck.belongsTo(User)
+
+Deck.hasMany(Card)
+Card.belongsTo(Deck)
+
+Card.hasMany(Attack)
+Attack.belongsToMany(Card)
+
+
+module.exports = { user, card, deck, attack }
